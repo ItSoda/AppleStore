be9e19c9cc15e1117f590547659d949dad791b6e
ItSoda <nikshchegilskiy@gmail.com>
2023-09-13 18:06:41 +0300

    second attempt

--- AppleStore/settings.py
+++ AppleStore/settings.py
@@ -154,10 +154,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-# STATIC_ROOT = 'static'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+STATIC_ROOT = 'static'
 
 # MEDIA
 MEDIA_URL = '/media/'
