915ed430c77bc2b9e470abdb9906efa92970758a
It Soda <nikshchegilskiy@gmail.com>
2023-10-19 21:09:56 +0300

    Authorization JWT

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
+
 
 from api.views import (BasketModelViewSet, CategoryModelViewSet,
                        OrderModelViewSet, ProductModelViewSet,
@@ -23,11 +25,17 @@ router.register(r'orders', OrderModelViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path('search/', ProductSearchView.as_view(), name='search-list'),
-    # Вся регистрация в двух строчках
+    
+    # Вся регистрация
     path(r'auth/', include('djoser.urls')),
-    path(r'auth/', include('djoser.urls.authtoken')),
     path("verify/<str:email>/<uuid:code>/", EmailVerificationAndUserUpdateView.as_view(), name='email_verify'),
-    path("update-user/", EmailVerificationAndUserUpdateView.as_view(), name='user-update'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path("update-user/", EmailVerificationAndUserUpdateView.as_view(), name='update-user'),
     path("order/create/", OrderCreateView.as_view(), name='order_create'),
     path("yookassa/webhook/", YookassaWebhookView.as_view(), name='yookassa_webhook'),
+
+
 ]
